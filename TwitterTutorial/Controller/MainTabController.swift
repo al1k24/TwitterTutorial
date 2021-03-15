0c3b354d6bb42378de464fcc98d5bdbf869b34cb
ALIK MOLDOVANU <al1k24@icloud.com>
2021-03-15 16:55:52 +0200

    Configure Project & Setup Root ViewController

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterTutorial
 //
 //  Created by Алик on 15.03.2021.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
 
-
 }
-
