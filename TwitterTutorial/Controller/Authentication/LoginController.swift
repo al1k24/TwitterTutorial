6857070661fba520501793b5d87fc01324373122
ALIK MOLDOVANU <al1k24@icloud.com>
2021-03-15 20:22:36 +0200

    Getting Started With Login UI

---
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginController.swift
+//  TwitterTutorial
+//
+//  Created by Алик on 15.03.2021.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    //MARK: - Properties
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = #imageLiteral(resourceName: "TwitterLogo")
+        return imageView
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+}
---
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,26 @@
+//
+//  RegistrationController.swift
+//  TwitterTutorial
+//
+//  Created by Алик on 15.03.2021.
+//
+
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
