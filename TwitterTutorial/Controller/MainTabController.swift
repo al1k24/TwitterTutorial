6474ef3f36b39d98c7d10df9dd5def8d5b1e3582
ALIK MOLDOVANU <al1k24@icloud.com>
2021-03-16 22:07:17 +0200

    Root ViewController Setup & Keeping User Logged In

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
     
@@ -20,12 +21,36 @@ class MainTabController: UITabBarController {
     }()
     
     //MARK: - Lifecycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configureViewControllers()
-        configureUI()
+//        logUserOut()
+        authenticateUserAndConfigureUI()
+        
+    }
+    
+    //MARK: - API
+    func authenticateUserAndConfigureUI() {
+        view.backgroundColor = .twitterBlue
+        
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else {
+            configureViewControllers()
+            configureUI()
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("* DEBUG: Failed to sign out with error \(error.localizedDescription)")
+        }
     }
     
     //MARK: - Selectors
