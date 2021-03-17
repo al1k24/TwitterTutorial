19337eb5e76fbadaebd8f4bbb37057905c3f99a3
ALIK MOLDOVANU <al1k24@icloud.com>
2021-03-17 12:42:02 +0200

    SDWebImage Install & Passing User To FeedController

--- TwitterTutorial/API/UserService.swift
+++ TwitterTutorial/API/UserService.swift
@@ -12,14 +12,14 @@ struct UserService {
     static let shared = UserService()
     private init() {}
     
-    func fetchUser() {
+    func fetchUser(completion: @escaping(User) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
 
             let user = User(uid: uid, dictionary: dictionary)
-            print("* DEBUG: UserName is \(user.userName)")
+            completion(user)
         }
     }
 }
