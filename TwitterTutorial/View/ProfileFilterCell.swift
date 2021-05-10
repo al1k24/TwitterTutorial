ca4b98f93496473b82e2ce1b459f92c2f8939c05
Alik Moldovanu <al1k24@icloud.com>
2021-05-10 20:30:46 +0300

    Profile Header Filter Bar

---
+++ TwitterTutorial/View/ProfileFilterCell.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileFilterCell.swift
+//  TwitterTutorial
+//
+//  Created by Alik on 10.05.2021.
+//
+
+import UIKit
+
+class ProfileFilterCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = .systemFont(ofSize: 14)
+        label.text = "Test"
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            titleLabel.font = isSelected ? .boldSystemFont(ofSize: 16) : .systemFont(ofSize: 14)
+            titleLabel.textColor = isSelected ? .twitterBlue : .lightGray
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        titleLabel.center(inView: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ TwitterTutorial/View/ProfileFilterView.swift
@@ -0,0 +1,73 @@
+//
+//  ProfileFilterView.swift
+//  TwitterTutorial
+//
+//  Created by Alik on 10.05.2021.
+//
+
+import UIKit
+
+private let reuseIdentifier = "ProfileFilterCell"
+
+class ProfileFilterView: UIView {
+    
+    // MARK: - Properties
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .white
+        collection.delegate = self
+        collection.dataSource = self
+        return collection
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        addSubview(collectionView)
+        collectionView.addConstraintsToFillView(self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfileFilterView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProfileFilterCell
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfileFilterView: UICollectionViewDelegate {
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 3, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+}
+
