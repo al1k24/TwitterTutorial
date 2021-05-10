15effd92a2bca1fba542b90ec7d7cdb4b4d3840b
Alik Moldovanu <al1k24@icloud.com>
2021-05-10 17:41:09 +0300

    Tweet Timestamp

--- TwitterTutorial/ViewModel/TweetViewModel.swift
+++ TwitterTutorial/ViewModel/TweetViewModel.swift
@@ -16,11 +16,26 @@ struct TweetViewModel {
         return tweet.user.profileImageUrl
     }
     
+    var timestamp: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekday]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let now = Date()
+        return formatter.string(from: tweet.timestamp, to: now) ?? "2m"
+    }
+    
     var userInfoText: NSAttributedString {
         let title = NSMutableAttributedString(string: user.fullName, attributes: [
             .font: UIFont.boldSystemFont(ofSize: 14)
         ])
+        
         title.append(NSAttributedString(string: " @\(user.userName)", attributes: [
+            .font: UIFont.systemFont(ofSize: 14),
+            .foregroundColor: UIColor.lightGray
+        ]))
+        
+        title.append(NSAttributedString(string: " · \(timestamp)", attributes: [
             .font: UIFont.systemFont(ofSize: 14),
             .foregroundColor: UIColor.lightGray
         ]))
